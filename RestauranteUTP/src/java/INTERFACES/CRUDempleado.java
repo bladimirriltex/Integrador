775370305f455dc24c493f5a69e05c691855d64c
1620298 <1620298@utp.edu.pe>
2020-07-09 18:04:56 -0500

    updatev2

--- RestauranteUTP/src/java/INTERFACES/CRUDempleado.java
+++ RestauranteUTP/src/java/INTERFACES/CRUDempleado.java
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package INTERFACES;
-
-
-import DTO.Empleado;
-import java.util.List;
-
-/**
- *
- * @author Gianpiero
- */
-public interface CRUDempleado {
-    public List listar();
-    public Empleado list(int id_empleado);
-    public boolean add(Empleado em);
-    public boolean edit(Empleado em);
-    public boolean eliminar(int id_empleado);
-    
-    
-}
-=======
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -41,5 +15,11 @@ import DTO.empleado;
  */
 public interface CRUDempleado {
     public empleado IniciarSesion(Persona persona);
+    public List listar();
+    public Empleado list(int id_empleado);
+    public boolean add(Empleado em);
+    public boolean edit(Empleado em);
+    public boolean eliminar(int id_empleado);
+    
 }
 >>>>>>> 4566e7b1de4b20a0c9097343399aef4080599f17
