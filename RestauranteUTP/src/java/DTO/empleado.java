0fc9bdadf30ebc0f368d686e9d5487e5cb723bde
1620298 <1620298@utp.edu.pe>
2020-07-09 20:11:43 -0500

    updatev4

---
+++ RestauranteUTP/src/java/DTO/empleado.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+/**
+ *
+ * @author Edú
+ */
+public class Empleado extends Persona{
+    private String ubicacion;
+    private String estado;
+    private int id_dist;
+    private int id_rol;
+    private int id_vehi;
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getId_dist() {
+        return id_dist;
+    }
+
+    public void setId_dist(int id_dist) {
+        this.id_dist = id_dist;
+    }
+
+    public int getId_rol() {
+        return id_rol;
+    }
+
+    public void setId_rol(int id_rol) {
+        this.id_rol = id_rol;
+    }
+
+    public int getId_vehi() {
+        return id_vehi;
+    }
+
+    public void setId_vehi(int id_vehi) {
+        this.id_vehi = id_vehi;
+    }
+    
+    
+}
